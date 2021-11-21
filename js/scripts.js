3ea4340884e971871658357db05ca45a3fa00039
Matthew Banse-Fay <matthew.bansefay@gmail.com>
2021-11-20 23:40:39 -0800

    Add user interface logic to display input number.

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,17 @@
 $(document).ready(function() {
   $("#input-form").submit(function(event) {
     event.preventDefault();
+    const inputNumber = parseInt($("#input-number").val());
+
+    if ($(".results-display").is(":hidden")) {
+      $("#results-display-span").text(inputNumber);
+      $(".results-display").slideToggle();
+    } else {
+      $("#results-display-span").slideUp("fast", function() {
+        $("#results-display-span").text(inputNumber);
+      });
+      $("#results-display-span").slideDown();
+    }
+    
   });
 });
